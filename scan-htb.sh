c2245c210f2a67a6fc0967c47345abdc115bdfbe
Vay3t <fffyanez@gmail.com>
2019-04-14 19:07:38 -0400

    Update scan-htb.sh

--- scan-htb.sh
+++ scan-htb.sh
@@ -2,6 +2,9 @@
 
 target=$1
 
+portsTCP="Null"
+portsUDP="Null"
+
 if [[ $(id -u) -ne 0 ]]; then
 	echo "[!] Please run as root"
 	exit 2
@@ -10,13 +13,35 @@ else
 		echo "[*] Usage: bash $0 <target>"
 		exit 1
 	else
-		if [[ $target =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
-			masscan $target -p1-65535,U:1-65535 -oG scan.grep --rate=1000 -e tun0 &> /dev/null
-			ports=$(grep open scan.grep | awk '{print $5}' | cut -d "/" -f1 | tr "\n" ",")
-			nmap $target -p $ports -n -T4 -sC -sV -Pn
+		ifconfig tun0 &> /dev/null
+		if [ $? -eq  0 ]; then
+			if [[ $target =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+				echo "[+] Discovering ports..."
+				masscan $target -p1-65535,U:1-65535 -oG out.grep --rate=1000 -e tun0 &> /dev/null
+				
+				openTCP=$(grep open out.grep | grep tcp)
+				if [ $? -eq 0 ]; then
+					portsTCP=$(echo $openTCP | awk '{print $5}' | cut -d "/" -f1 | tr "\n" ",")
+				fi	
+				openUDP=$(grep open out.grep | grep udp)
+				if [ $? -eq 0 ]; then
+					portsUDP=$(echo $openUDP | awk '{print $5}' | cut -d "/" -f1 | tr "\n" ",")
+				fi
+				if [ $portsTCP != "Null" ]; then
+					echo "[+] Scan TCP ports"
+					nmap $target -p $portsTCP -n -T4 -sC -sV -Pn
+				fi
+				if [ $portsUDP != "Null" ]; then
+					echo "[+] Scan UDP ports"
+					nmap $target -sU -p $portsUDP -n -T4 -sC -sV -Pn
+				fi
+				echo "[*] Finished"
+			else
+				echo "[!] Invalid IP"
+				exit 2
+			fi
 		else
-			echo "[!] Invalid IP"
-			exit 2
-		fi
+			echo "[-] You are not connected to hackthebox"
+		fi	
 	fi
 fi
