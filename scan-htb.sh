e4a371f545cbad0c6382deaa0a576641fcdf3f82
Vay3t <fffyanez@gmail.com>
2019-04-19 11:22:56 -0400

    fixed big problem

--- scan-htb.sh
+++ scan-htb.sh
@@ -17,19 +17,21 @@ else
 		if [ $? -eq  0 ]; then
 			if [[ $target =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
 				echo "[+] Discovering ports..."
-				masscan $target -p1-65535,U:1-65535 -oG out.grep --rate=1000 -e tun0 &> /dev/null
-				
-				openTCP=$(grep open out.grep | grep tcp)
+				masscan $target -p1-65535,U:1-65535 -oG out.grep --rate=1000 -e tun0 
+
+				grep open out.grep | grep tcp > tmptcp.grep
 				if [ $? -eq 0 ]; then
-					portsTCP=$(echo $openTCP | awk '{print $5}' | cut -d "/" -f1 | tr "\n" ",")
-				fi	
-				openUDP=$(grep open out.grep | grep udp)
-				if [ $? -eq 0 ]; then
-					portsUDP=$(echo $openUDP | awk '{print $5}' | cut -d "/" -f1 | tr "\n" ",")
+					portsTCP=$(cat tmptcp.grep | awk '{print $5}' | cut -d "/" -f1 | tr "\n" ",")
 				fi
+
+				grep open out.grep | grep udp > tmpudp.grep
+				if [ $? -eq 0 ]; then
+					portsUDP=$(cat tmpudp.grep | awk '{print $5}' | cut -d "/" -f1 | tr "\n" ",")
+				fi
+
 				if [ $portsTCP != "Null" ]; then
 					echo "[+] Scan TCP ports"
-					nmap $target -p $portsTCP -n -T4 -sC -sV -Pn
+					nmap $target -p $portsTCP -v -n -T4 -sC -sV -Pn
 				fi
 				if [ $portsUDP != "Null" ]; then
 					echo "[+] Scan UDP ports"
@@ -42,6 +44,6 @@ else
 			fi
 		else
 			echo "[-] You are not connected to hackthebox"
-		fi	
+		fi
 	fi
 fi
